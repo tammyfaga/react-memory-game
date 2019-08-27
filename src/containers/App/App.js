5b79114a93e59b20cfcfb1e8c330c06eeb063e88
tammy <tmata@recordtv.com.br>
2019-08-26 23:27:53 -0300

    tarefa: Adiciona Redux, redux DevTools e cria store.

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { Provider as ReduxProvider } from "react-redux";
 
 import Routes from '../Routes';
+import store from "../../store";
 
 const App = () => (
-  <>
-  <CssBaseline />
-  <Router>
-    <Routes />
-  </Router>
-  </>
+  <ReduxProvider store={store}>
+    <>
+    <CssBaseline />
+    <Router>
+      <Routes />
+    </Router>
+    </>
+  </ReduxProvider>
 );
 
 export default App;
